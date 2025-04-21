16681103c563e2edf13fc281139858f27742fff8
Omar Mattos <omarmatt84@gmail.com>
2025-04-21 20:32:41 -0300

    se dibujo la tabla del ejercicio 10 del tp

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,10 +1,14 @@
 const fila=parseInt(prompt("Ingrese el número de filas de la tabla"))
 const columna=parseInt(prompt("Ingrese el número de columnas de la tabla"))
 let numero=fila*columna
-for(i=fila;i>0;i--){
+document.writeln(`<table><tbody>`)
+for(i=fila;i>0;i--){document.writeln(`<tr>`)
     for(j=columna;j>0;j--){
+        document.writeln(`<td>`)
         document.writeln(numero)
+        document.writeln(`</td>`)
         numero--
     }
-    document.writeln("<br>")
+    document.writeln(`</tr>`)
 }
+document.writeln(`</tbody> </table>`)
---
+++ js/ejercicio11.js
@@ -0,0 +1,7 @@
+const nombre1=prompt("Ingrese el nnombre de la primera persona")
+const edad1=prompt("Ingrese la edad de la primera persona")
+const nombre2=prompt("Ingrese el nnombre de la segunda persona")
+const edad2=prompt("Ingrese la edad de la segunda persona")
+const nombre3=prompt("Ingrese el nnombre de la tercera persona")
+const edad3=prompt("Ingrese la edad de la tercera persona")
+
