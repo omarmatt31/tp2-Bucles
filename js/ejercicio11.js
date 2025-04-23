446edc42560b30d40291732dd7eb428368acf542
Omar Mattos <omarmatt84@gmail.com>
2025-04-22 23:36:15 -0300

    se realizó el ejercicio 11 con control de datos

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -5,15 +5,18 @@ const edad2=parseInt(prompt("Ingrese la edad de la segunda persona"))
 const nombre3=prompt("Ingrese el nombre de la tercera persona")
 const edad3=parseInt(prompt("Ingrese la edad de la tercera persona"))
 
-
-switch(Math.max(edad1, edad2, edad3)){
-    case edad1:
-        document.writeln(nombre1)
-        break;
-    case edad2:
-        document.writeln(nombre2)
-        break;
-    case edad3:
-        document.writeln(nombre3)
-        break;
+if(edad1>=0 && edad2>=0 && edad3>=0){
+    switch(Math.max(edad1, edad2, edad3)){
+        case edad1:
+            document.writeln(nombre1)
+            break;
+        case edad2:
+            document.writeln(nombre2)
+            break;
+        case edad3:
+            document.writeln(nombre3)
+            break;
+    }
+}else {
+    alert("Datos invalidos")
 }
