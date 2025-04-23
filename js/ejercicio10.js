42136e37b2b985a53d9297736a8da1e8df2fe7b4
Omar Mattos <omarmatt84@gmail.com>
2025-04-22 23:33:26 -0300

    se realizó el ejercicio 11

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -2,8 +2,8 @@ const fila=parseInt(prompt("Ingrese el número de filas de la tabla"))
 const columna=parseInt(prompt("Ingrese el número de columnas de la tabla"))
 let numero=fila*columna
 document.writeln(`<table><tbody>`)
-for(i=fila;i>0;i--){document.writeln(`<tr>`)
-    for(j=columna;j>0;j--){
+for(let i=0;i<fila;i++){document.writeln(`<tr>`)
+    for(let j=0;j<columna;j++){
         document.writeln(`<td>`)
         document.writeln(numero)
         document.writeln(`</td>`)
--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,7 +1,19 @@
-const nombre1=prompt("Ingrese el nnombre de la primera persona")
-const edad1=prompt("Ingrese la edad de la primera persona")
-const nombre2=prompt("Ingrese el nnombre de la segunda persona")
-const edad2=prompt("Ingrese la edad de la segunda persona")
-const nombre3=prompt("Ingrese el nnombre de la tercera persona")
-const edad3=prompt("Ingrese la edad de la tercera persona")
+const nombre1=prompt("Ingrese el nombre de la primera persona")
+const edad1=parseInt(prompt("Ingrese la edad de la primera persona"))
+const nombre2=prompt("Ingrese el nombre de la segunda persona")
+const edad2=parseInt(prompt("Ingrese la edad de la segunda persona"))
+const nombre3=prompt("Ingrese el nombre de la tercera persona")
+const edad3=parseInt(prompt("Ingrese la edad de la tercera persona"))
 
+
+switch(Math.max(edad1, edad2, edad3)){
+    case edad1:
+        document.writeln(nombre1)
+        break;
+    case edad2:
+        document.writeln(nombre2)
+        break;
+    case edad3:
+        document.writeln(nombre3)
+        break;
+}
